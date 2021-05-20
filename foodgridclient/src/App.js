6122121f52d7e916fa608e156fb59b3acc93ae1d
Marc Ranger <marc.m.ranger@gmail.com>
2021-05-20 08:52:48 -0600

    refactored to use React Router

--- foodgridclient/src/App.js
+++ foodgridclient/src/App.js
@@ -1,20 +1,45 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //import axios from "axios";
 import AddItem from "./components/AddItem";
 import ListItems from "./components/ListItems";
+import Home from "./components/Home";
 
 function App() {
-  const [viewMode, setViewMode] = useState("ListView");
   return (
-    <div className="App">
-      <h1>FoodGrid Application</h1>
-      <button onClick={() => setViewMode("AddView")}>Add New Item</button>
-      <button onClick={() => setViewMode("ListView")}>View List</button>
-      {viewMode === "AddView" && <AddItem />}
-      {viewMode === "ListView" && <ListItems />}
-    </div>
+    <>
+      <Router>
+        <div
+          style={{
+            width: 100 + "vm",
+            height: 80,
+            backgroundColor: "lightblue",
+          }}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/listitems">ListItems</Link>
+          <Link to="/additem">AddItem</Link>
+        </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/listitems" exact component={ListItems} />
+          <Route path="/additem" exact component={AddItem} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+// const [viewMode, setViewMode] = useState("ListView");
+// return (
+//   <div className="App">
+//     <h1>FoodGrid Application</h1>
+//     <button onClick={() => setViewMode("AddView")}>Add New Item</button>
+//     <button onClick={() => setViewMode("ListView")}>View List</button>
+//     {viewMode === "AddView" && <AddItem />}
+//     {viewMode === "ListView" && <ListItems />}
+//   </div>
+// );
